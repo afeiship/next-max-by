0ac2b2d4e02cdba1f7a3fb35c4fc4654968c4614
afeiship <1290657123@qq.com>
2024-05-30 14:30:45 +0800

    fix: typing err

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
-type MayByString = (array: any[], key: string) => any;
-type MayByFunction = (array: any[], key: (value: any) => any) => any;
-
 interface NxStatic {
-  maxBy: MayByFunction & MayByString;
+  maxBy(array: any[], key: string): any;
+  maxBy(array: any[], key: (value: any) => any): any;
 }
